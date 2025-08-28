a05442b06b077584db3bca5f0da5dd49468b0d08
TamaraKosovac <tamarakosovac14@gmail.com>
2025-08-29 00:35:34 +0200

    Rss promotions

---
+++ ERent/backend/src/main/java/org/unibl/etf/ip/erent/dto/RssPromotionDTO.java
@@ -0,0 +1,15 @@
+package org.unibl.etf.ip.erent.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+public class RssPromotionDTO {
+    public String title;
+    public String description;
+    public LocalDate startDate;
+    private LocalDate endDate;
+}
