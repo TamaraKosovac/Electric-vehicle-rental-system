b7eebefc0ba09a45e452504f14c4ec4d52e03896
TamaraKosovac <tamarakosovac14@gmail.com>
2025-08-29 17:49:15 +0200

    Staff-app login

---
+++ ERent/frontend/staff-app/src/app/pages/login/login.component.ts
@@ -0,0 +1,44 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [CommonModule, FormsModule, MatFormFieldModule, MatInputModule, MatButtonModule],
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+  username = '';
+  password = '';
+  errorMessage = '';
+
+  constructor(private authService: AuthService, private router: Router) {}
+
+  login() {
+    this.authService.login(this.username, this.password).subscribe({
+      next: (response) => {
+        console.log('Login uspješan:', response);
+        localStorage.setItem('user', JSON.stringify(response));
+        if (response.role === 'ADMIN') {
+          this.router.navigate(['/admin']);
+        } else if (response.role === 'OPERATOR') {
+          this.router.navigate(['/operator']);
+        } else if (response.role === 'MANAGER') {
+          this.router.navigate(['/manager']);
+        } else {
+          this.errorMessage = 'Nepoznata uloga!';
+        }
+      },
+      error: () => {
+        this.errorMessage = 'Pogrešan username ili password!';
+      }
+    });
+  }
+}
