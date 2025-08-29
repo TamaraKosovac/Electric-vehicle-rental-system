9de94bd278d00f545a4fc9428950111cdfbcffd7
TamaraKosovac <tamarakosovac14@gmail.com>
2025-08-29 17:49:15 +0200

    Staff-app login

--- ERent/frontend/staff-app/src/app/app.routes.ts
+++ ERent/frontend/staff-app/src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { AdminComponent } from './pages/admin/admin.component';
+import { OperatorComponent } from './pages/operator/operator.component';
+import { ManagerComponent } from './pages/manager/manager.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'admin', component: AdminComponent },
+  { path: 'operator', component: OperatorComponent },
+  { path: 'manager', component: ManagerComponent },
+  { path: '', redirectTo: 'login', pathMatch: 'full' }
+];
