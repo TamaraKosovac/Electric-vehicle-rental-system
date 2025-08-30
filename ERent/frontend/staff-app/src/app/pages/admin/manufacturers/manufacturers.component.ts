78e5c0928d2d31a4d303c81c4fd0295e652b1000
TamaraKosovac <tamarakosovac14@gmail.com>
2025-08-31 00:08:34 +0200

    CRUD for manufacturers in staff-app

--- ERent/frontend/staff-app/src/app/pages/admin/manufacturers/manufacturers.component.ts
+++ ERent/frontend/staff-app/src/app/pages/admin/manufacturers/manufacturers.component.ts
@@ -1,11 +1,56 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatTableModule } from '@angular/material/table';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule } from '@angular/forms';
+import { ManufacturersService } from '../../../services/manufacturers.service';
 
 @Component({
   selector: 'app-manufacturers',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, MatTableModule, MatButtonModule, MatInputModule, FormsModule],
   templateUrl: './manufacturers.component.html',
-  styleUrl: './manufacturers.component.css'
+  styleUrls: ['./manufacturers.component.css']
 })
-export class ManufacturersComponent {
+export class ManufacturersComponent implements OnInit {
+  manufacturers: any[] = [];
+  displayedColumns: string[] = ['id', 'name', 'country', 'address', 'contact', 'actions'];
 
+  newManufacturer: any = { name: '', country: '', address: '', contact: '' };
+  editing: any = null;
+
+  constructor(private manufacturersService: ManufacturersService) {}
+
+  ngOnInit() {
+    this.loadManufacturers();
+  }
+
+  loadManufacturers() {
+    this.manufacturersService.getAll().subscribe(data => this.manufacturers = data);
+  }
+
+  addManufacturer() {
+    this.manufacturersService.create(this.newManufacturer).subscribe(() => {
+      this.newManufacturer = { name: '', country: '', address: '', contact: '' };
+      this.loadManufacturers();
+    });
+  }
+
+  editManufacturer(m: any) {
+    this.editing = { ...m };
+  }
+
+  updateManufacturer() {
+    this.manufacturersService.update(this.editing.id, this.editing).subscribe(() => {
+      this.editing = null;
+      this.loadManufacturers();
+    });
+  }
+
+  deleteManufacturer(id: number) {
+    this.manufacturersService.delete(id).subscribe(() => {
+      this.manufacturers = this.manufacturers.filter(m => m.id !== id);
+    });
+  }
 }
