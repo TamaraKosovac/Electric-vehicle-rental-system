a3a728c556e56b0ec9b5ecba5a7bd35e1abcc020
TamaraKosovac <tamarakosovac14@gmail.com>
2025-08-30 11:53:40 +0200

    Vehicle-details page

--- ERent/frontend/staff-app/src/app/pages/admin/vehicle-details/vehicle-details.component.ts
+++ ERent/frontend/staff-app/src/app/pages/admin/vehicle-details/vehicle-details.component.ts
@@ -1,11 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { VehiclesService } from '../../../services/vehicles.service';
 
 @Component({
   selector: 'app-vehicle-details',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './vehicle-details.component.html',
-  styleUrl: './vehicle-details.component.css'
+  styleUrls: ['./vehicle-details.component.css']
 })
-export class VehicleDetailsComponent {
+export class VehicleDetailsComponent implements OnInit {
+  vehicleType!: string;
+  vehicleId!: number;
+  vehicle: any;
+  malfunctions: any[] = [];
+  rentals: any[] = [];
 
+  constructor(
+    private route: ActivatedRoute,
+    private vehiclesService: VehiclesService
+  ) {}
+
+  ngOnInit(): void {
+    this.vehicleType = this.route.snapshot.paramMap.get('type')!;  // cars | bikes | scooters
+    this.vehicleId = Number(this.route.snapshot.paramMap.get('id'));
+
+    this.vehiclesService.getVehicleById(this.vehicleType, this.vehicleId)
+      .subscribe(v => this.vehicle = v);
+
+    this.vehiclesService.getMalfunctions(this.vehicleType, this.vehicleId)
+      .subscribe(m => this.malfunctions = m);
+
+    this.vehiclesService.getRentals(this.vehicleType, this.vehicleId)
+      .subscribe(r => this.rentals = r);
+  }
+
+  addMalfunction(desc: string) {
+    this.vehiclesService.addMalfunction(this.vehicleType, this.vehicleId, { description: desc })
+      .subscribe(newM => this.malfunctions.push(newM));
+  }
+
+  deleteMalfunction(id: number) {
+    this.vehiclesService.deleteMalfunction(this.vehicleType, id).subscribe(() => {
+      this.malfunctions = this.malfunctions.filter(m => m.id !== id);
+    });
+  }
 }
