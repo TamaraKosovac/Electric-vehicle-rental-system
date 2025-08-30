00d89732343c9ee34c066ba4bc96a24829d04a33
TamaraKosovac <tamarakosovac14@gmail.com>
2025-08-31 00:20:12 +0200

    CRUD for employees in staff-app

--- ERent/frontend/staff-app/src/app/pages/admin/users/users.component.ts
+++ ERent/frontend/staff-app/src/app/pages/admin/users/users.component.ts
@@ -1,11 +1,74 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatTableModule } from '@angular/material/table';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule } from '@angular/forms';
+import { UsersService } from '../../../services/users.service';
 
 @Component({
   selector: 'app-users',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, MatTabsModule, MatTableModule, MatButtonModule, MatInputModule, FormsModule],
   templateUrl: './users.component.html',
-  styleUrl: './users.component.css'
+  styleUrls: ['./users.component.css']
 })
-export class UsersComponent {
+export class UsersComponent implements OnInit {
+  clients: any[] = [];
+  employees: any[] = [];
+  displayedClientColumns: string[] = ['id', 'username', 'email', 'blocked', 'actions'];
+  displayedEmployeeColumns: string[] = ['id', 'username', 'role', 'actions'];
 
+  newEmployee: any = { username: '', password: '', firstName: '', lastName: '', role: '' };
+  editingEmployee: any = null;
+
+  constructor(private usersService: UsersService) {}
+
+  ngOnInit() {
+    this.loadClients();
+    this.loadEmployees();
+  }
+
+  // --- Clients ---
+  loadClients() {
+    this.usersService.getClients().subscribe(data => this.clients = data);
+  }
+
+  blockClient(id: number) {
+    this.usersService.blockClient(id).subscribe(() => this.loadClients());
+  }
+
+  unblockClient(id: number) {
+    this.usersService.unblockClient(id).subscribe(() => this.loadClients());
+  }
+
+  // --- Employees ---
+  loadEmployees() {
+    this.usersService.getEmployees().subscribe(data => this.employees = data);
+  }
+
+  addEmployee() {
+    this.usersService.createEmployee(this.newEmployee).subscribe(() => {
+      this.newEmployee = { username: '', password: '', firstName: '', lastName: '', role: '' };
+      this.loadEmployees();
+    });
+  }
+
+  editEmployee(e: any) {
+    this.editingEmployee = { ...e };
+  }
+
+  updateEmployee() {
+    this.usersService.updateEmployee(this.editingEmployee.id, this.editingEmployee).subscribe(() => {
+      this.editingEmployee = null;
+      this.loadEmployees();
+    });
+  }
+
+  deleteEmployee(id: number) {
+    this.usersService.deleteEmployee(id).subscribe(() => {
+      this.employees = this.employees.filter(e => e.id !== id);
+    });
+  }
 }
---
+++ ERent/frontend/staff-app/src/app/services/users.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UsersService {
+  private baseUrl = 'http://localhost:8080/api';
+
+  constructor(private http: HttpClient) {}
+
+  getClients(): Observable<any[]> {
+    return this.http.get<any[]>(`${this.baseUrl}/clients`);
+  }
+
+  blockClient(id: number) {
+    return this.http.put(`${this.baseUrl}/clients/${id}/block`, {});
+  }
+
+  unblockClient(id: number) {
+    return this.http.put(`${this.baseUrl}/clients/${id}/unblock`, {});
+  }
+
+  getEmployees(): Observable<any[]> {
+    return this.http.get<any[]>(`${this.baseUrl}/employees`);
+  }
+
+  createEmployee(employee: any) {
+    return this.http.post(`${this.baseUrl}/employees`, employee);
+  }
+
+  updateEmployee(id: number, employee: any) {
+    return this.http.put(`${this.baseUrl}/employees/${id}`, employee);
+  }
+
+  deleteEmployee(id: number) {
+    return this.http.delete(`${this.baseUrl}/employees/${id}`);
+  }
+}
