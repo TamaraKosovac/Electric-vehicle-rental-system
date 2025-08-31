4f407f2c439a790386eaf4b1d467deb997fbbb42
TamaraKosovac <tamarakosovac14@gmail.com>
2025-08-31 23:01:22 +0200

    Added side menu

--- ERent/frontend/staff-app/src/app/app.routes.ts
+++ ERent/frontend/staff-app/src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
+
 import { LoginComponent } from './pages/login/login.component';
-import { AdminDashboardComponent } from './pages/admin/admin-dashboard.component';
+import { AdminDashboardComponent } from './pages/admin/admin-dashboard/admin-dashboard.component';
 import { OperatorDashboardComponent } from './pages/operator/operator-dashboard.component';
 import { ManagerDashboardComponent } from './pages/manager/manager-dashboard.component';
 
@@ -11,17 +12,22 @@ import { UsersComponent } from './pages/admin/users/users.component';
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
+
   {
     path: 'admin',
     component: AdminDashboardComponent,
     children: [
+      { path: '', pathMatch: 'full', redirectTo: 'vehicles' }, 
       { path: 'vehicles', component: VehiclesComponent },
       { path: 'vehicles/:type/:id', component: VehicleDetailsComponent },
       { path: 'manufacturers', component: ManufacturersComponent },
       { path: 'users', component: UsersComponent }
     ]
   },
+
   { path: 'operator', component: OperatorDashboardComponent },
   { path: 'manager', component: ManagerDashboardComponent },
-  { path: '', redirectTo: 'login', pathMatch: 'full' }
+
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: '**', redirectTo: 'login' } 
 ];
