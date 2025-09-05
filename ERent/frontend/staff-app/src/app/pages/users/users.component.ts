20458d7e2b5a8d0be18bb73d77ec1307a0772c06
TamaraKosovac <tamarakosovac14@gmail.com>
2025-09-05 16:52:43 +0200

    Updated clients section for operators and managers

--- ERent/frontend/staff-app/src/app/pages/users/users.component.ts
+++ ERent/frontend/staff-app/src/app/pages/users/users.component.ts
@@ -16,7 +16,6 @@ import { EmployeeRole } from '../../models/enums/employee-role.enum';
 import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 import { UserFormComponent } from './user-form/user-form.component';
 import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
-import { AuthService } from '../../services/auth.service';
 
 
 @Component({
@@ -61,12 +60,10 @@ export class UsersComponent implements OnInit {
   };
 
   editingEmployee: Employee | null = null;
-  userRole: string | null = null;
 
   constructor(private usersService: UsersService,
     private dialog: MatDialog,
-    private snackBar: MatSnackBar,
-    private authService: AuthService
+    private snackBar: MatSnackBar
   ) {}
 
   activeTab: 'clients' | 'employees' = 'clients';
@@ -78,13 +75,8 @@ export class UsersComponent implements OnInit {
   }
 
   ngOnInit() {
-    const user = this.authService.getUser();
-    this.userRole = user?.role?.toLowerCase() || null;
-
     this.loadClients();
-    if (this.userRole === 'admin' || this.userRole === 'manager') {
-      this.loadEmployees();
-    }
+    this.loadEmployees();
   }
 
   loadClients() {
